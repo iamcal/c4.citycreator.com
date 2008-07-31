4f7ae1caa40da98afd3147c323c4a77d39546c15
Cal Henderson <cal@iamcal.com>
2008-07-31 22:21:57 +0000

    pottering

--- rebuild.php
+++ rebuild.php
@@ -10,17 +10,42 @@
 	# TODO: optimize this to not save props to DB unless they changed
 	#
 
-	$result = db_query("SELECT * FROM user_buildings");
+
+	#
+	# loop over each city
+	#
+
+	$result = db_query("SELECT * FROM users");
 	while ($row = db_fetch_hash($result)){
 
-		$i = init_building_instance($row);
-
-		$i->b->on_refresh($i);
-
-		$i->save_local_props();
-		$i->save_global_props();
+		echo "rebuilding user $row[email]...<br />\n";
+		local_rebuild_user($row);
+		echo "done<br />\n";
 	}
 
-	echo "done!";
+
+
+	#
+	# process all buildings in a city
+	#
+
+	function local_rebuild_user($user){
+
+		login_set_user($user);
+
+		$result = db_query("SELECT * FROM user_buildings WHERE user_id=$user[id]");
+		while ($row = db_fetch_hash($result)){
+
+			$i = init_building_instance($row);
+
+			$i->b->on_refresh($i);
+
+			$i->save_local_props();
+		}
+
+		login_save_global_props();
+	}
+
+	echo "all done!";
 
 ?>
