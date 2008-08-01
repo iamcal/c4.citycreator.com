5137767c796e2938244007e5781338c6d9bdd6d2
Cal Henderson <cal@iamcal.com>
2008-08-01 18:48:24 +0000

    more admin work

--- admin/building_run.php
+++ admin/building_run.php
@@ -21,12 +21,26 @@
 
 
 	#
-	# run build process
+	# how much extra blue to leave above the tile previews
+	#
+
+	$extra_height = 50;
+
+	$smarty->assign('extra_height', $extra_height);
+
+
+	#
+	# create a building
 	#
 
 	$b =& load_building($_REQUEST[id]);
 	$i =& new play_building_instance();
 	$i->id = 'test';
+
+
+	#
+	# build the building
+	#
 
 	$b->on_design($i);
 
@@ -36,67 +50,99 @@
 
 	eval($_POST[custom]); # second time to apply post-build effects
 
-	$b->on_refresh($i);
-
-	$smarty->assign_by_ref('instance', $i);
-
 
 	#
-	# how much extra blue to leave above the tile preview
+	# store build process information
 	#
 
-	$extra_height = 50;
-
-	$smarty->assign('extra_height', $extra_height);
-
-
-	#
-	# grab the tile data
-	#
-
-	$tileset = array(
-		'w'	=> $i->get_local_prop('size_x'),
-		'h'	=> $i->get_local_prop('size_y'),
-		'tiles'	=> $i->get_local_prop('tileset'),
+	$build_info = array(
+		'build_time'	=> $i->props[build_time],
+		'build_ticks'	=> $i->props[build_ticks],
+		'builders'	=> $i->props[builders],
+		'materials'	=> $i->props[materials],
 	);
 
-	$smarty->assign_by_ref('tileset', $tileset);
+	$smarty->assign_by_ref('build_info', $build_info);
 
 
 	#
-	# calculate base tile positions
+	# store state info once per build tick
 	#
 
-	$base_tiles = array();
+	$step_props = array();
 
-	$bounds = map_bounding_box(0, 0, $tileset[w]-1, $tileset[h]-1);
+	$smarty->assign_by_ref('step_props', $step_props);
 
-	$bounds[1] -= $extra_height;
 
-	for ($x=0; $x<$tileset[w]; $x++){
-		for ($y=0; $y<$tileset[h]; $y++){
+	for ($j=0; $j<=$build_info[build_ticks]; $j++){
 
-			$pos = map_tile_position($x, $y);
+		$i->props[build_tick] = $j;
+		$i->props[built] = $j == $build_info[build_ticks] ? 1 : 0;
 
-			$base_tiles[] = array(
-				'xp'	=> $pos[0] - $bounds[0],
-				'yp'	=> $pos[1] - $bounds[1],
-				'x'	=> $x,
-				'y'	=> $y,
-				'up_x'	=> $pos[0] - $bounds[0],
-				'up_y'	=> 0,
-				'up_w'	=> 56,
-				'up_h'	=> 14 + $pos[1] - $bounds[1],
-			);
-		}
+		$b->on_refresh($i);
+
+		$step_props[$j] = array(
+			'props'		=> $i->props,
+			'globals'	=> $cfg[global_props][test],
+			'tileset'	=> local_build_tileset($i->props),
+		);
 	}
 
-	$base_w = $bounds[2] - $bounds[0];
-	$base_h = $bounds[3] - $bounds[1];
 
-	$smarty->assign('base_tiles', $base_tiles);
-	$smarty->assign('base_w', $base_w);
-	$smarty->assign('base_h', $base_h);
+	#
+	# function to prep a tileset for a given propset
+	#
+
+	function local_build_tileset(&$props){
+
+		#
+		# grab the tile data
+		#
+
+		$tileset = array(
+			'w'	=> $props['size_x'],
+			'h'	=> $props['size_y'],
+			'tiles'	=> $props['tileset'],
+		);
+
+
+		#
+		# calculate base tile positions
+		#
+
+		$base_tiles = array();
+
+		$bounds = map_bounding_box(0, 0, $tileset[w]-1, $tileset[h]-1);
+
+		$bounds[1] -= $GLOBALS[extra_height];
+
+		for ($x=0; $x<$tileset[w]; $x++){
+			for ($y=0; $y<$tileset[h]; $y++){
+
+				$pos = map_tile_position($x, $y);
+
+				$base_tiles[] = array(
+					'xp'	=> $pos[0] - $bounds[0],
+					'yp'	=> $pos[1] - $bounds[1],
+					'x'	=> $x,
+					'y'	=> $y,
+					'up_x'	=> $pos[0] - $bounds[0],
+					'up_y'	=> 0,
+					'up_w'	=> 56,
+					'up_h'	=> 14 + $pos[1] - $bounds[1],
+				);
+			}
+		}
+
+		$base_w = $bounds[2] - $bounds[0];
+		$base_h = $bounds[3] - $bounds[1];
+
+		$tileset[base_tiles] = $base_tiles;
+		$tileset[base_w] = $base_w;
+		$tileset[base_h] = $base_h;
+
+		return $tileset;
+	}
 
 
 
