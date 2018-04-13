21b48533bcfb14b261f5400128538a9141b55da8
Cal Henderson <cal@iamcal.com>
2018-04-13 17:45:09 -0500

    missing tables

---
+++ db/citycreator_cards.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6cbe01c868688f7a3f88aa2554067b95a82c4034afd129512263ec8a932e3fc
+size 2960133
---
+++ db/citycreator_suggestions.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d23b3629e4801f7197fe4350578168c8b0cd3ced477a31090e604e47a361280
+size 28011687
