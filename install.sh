adbda1199fbaf9fbe0e8847597aef42214b8b3c2
Cal Henderson <cal@iamcal.com>
2018-04-15 16:08:32 -0500

    neuron-base installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /var/www/html/citycreator.com
+(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-32};echo) > secrets/session_crypto_key
+(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-40};echo) > secrets/duo_app_key
+
+ln -s /var/www/html/citycreator.com/site.conf /etc/apache2/sites-available/citycreator.com.conf
+a2ensite citycreator.com
+service apache2 reload
+
+cd db
+./init_db.sh
+
